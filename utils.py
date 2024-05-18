2566568f3b128357a301893769a6132147d0e2c7
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-18 19:18:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM
+from info import *
 from imdb import IMDb
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
@@ -8,7 +9,8 @@ from pyrogram import enums
 from typing import Union
 import re
 import os
-from datetime import datetime
+import pytz
+from datetime import datetime, timedelta, date, time
 from typing import List
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
@@ -23,6 +25,9 @@ BTN_URL_REGEX = re.compile(
 
 imdb = IMDb() 
 
+TOKENS = {}
+VERIFIED = {}
+URLINK = {}
 BANNED = {}
 SMART_OPEN = '“'
 SMART_CLOSE = '”'
@@ -39,6 +44,11 @@ class temp(object):
     U_NAME = None
     B_NAME = None
     SETTINGS = {}
+    VERIFY = {}
+    ACTIVE_URL = {}
+    TOKEN_ACCEPTED = {}
+    STORE_ID = {}
+    
 
 async def is_subscribed(bot, query):
     try:
@@ -375,3 +385,250 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+async def send_verification_log(bot, userid, short_temp, date_temp, time_temp):
+    user = await bot.get_users(int(userid))
+    url = URLINK[user.id]
+    if short_temp == 1:
+        shortnum = 4
+    else:
+        shortnum = short_temp - 1
+    log_message = f"#VerificationLog:\nUser ID: {user.id}\nUser Name: {user.mention}\nShortNum: {shortnum}\nDate: {date_temp}\nTime: {time_temp}\nUrl: {url}"
+    update_message = f"/update {user.id}_{shortnum}"
+    await bot.send_message(LOG_CHANNEL, log_message)
+    await bot.send_message(LOG_CHAT, update_message)
+
+
+async def update_verify_status(bot, userid, short_temp, date_temp, time_temp):
+    status = await get_verify_status(userid)
+    status["short"] = short_temp
+    status["date"] = date_temp
+    status["time"] = time_temp
+    temp.VERIFY[userid] = status
+    await db.update_verification(userid, short_temp, date_temp, time_temp)
+    await send_verification_log(bot, userid, short_temp, date_temp, time_temp)
+
+
+async def verify_user(bot, userid, token):
+    user = await bot.get_users(int(userid))
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention()))
+    TOKENS[user.id] = {token: True}
+    status = await get_verify_status(user.id)
+    tz = pytz.timezone('Asia/Kolkata')
+    short_var = status["short"]
+    shortnum = int(short_var)
+    if shortnum == 4:
+        vrnum = 1
+        date_var = datetime.now(tz) + timedelta(hours=24)
+    else:
+        vrnum = shortnum + 1
+        date_var = datetime.now(tz) - timedelta(hours=25)
+    temp_time = date_var.strftime("%H:%M:%S")
+    date_var, time_var = str(date_var).split(" ")
+    await update_verify_status(bot, user.id, vrnum, date_var, temp_time)
+
+
+async def check_token(bot, userid, token):
+    user = await bot.get_users(userid)
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    if user.id in TOKENS.keys():
+        TKN = TOKENS[user.id]
+        if token in TKN.keys():
+            is_used = TKN[token]
+            if is_used == True:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+
+async def get_verify_shorted_link_first(num, link):
+    if int(num) == 1:
+        API = SHORT1_API
+        URL = SHORT1_URL
+    elif int(num) == 2:
+        API = SHORT2_API
+        URL = SHORT2_URL
+    elif int(num) == 3:
+        API = SHORT3_API
+        URL = SHORT3_URL
+    else:
+        API = SHORT4_API
+        URL = SHORT4_URL
+    
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+
+    if URL == "api.shareus.in":
+        url = f"https://{URL}/shortLink"
+        params = {"token": API,
+                  "format": "json",
+                  "link": link,
+                  }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json(content_type="text/html")
+                    if data["status"] == "success":
+                        return data["shortlink"]
+                    else:
+                        logger.error(f"Error: {data['message']}")
+                        return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+
+        except Exception as e:
+            logger.error(e)
+            return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+    else:
+        url = f'https://{URL}/api'
+        params = {'api': API,
+                  'url': link,
+                  }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json()
+                    if data["status"] == "success":
+                        return data["shortenedUrl"]
+                    else:
+                        logger.error(f"Error: {data['message']}")
+                        if URL == 'clicksfly.com':
+                            return f'https://{URL}/api?api={API}&url={link}'
+                        else:
+                            return f'https://{URL}/api?api={API}&link={link}'
+        except Exception as e:
+            logger.error(e)
+            if URL == 'clicksfly.com':
+                return f'https://{URL}/api?api={API}&url={link}'
+            else:
+                return f'https://{URL}/api?api={API}&link={link}'
+
+
+async def get_verify_shorted_link_second(num, link):
+    if int(num) == 1:
+        API = VERIFY1_API
+        URL = VERIFY1_URL
+    elif int(num) == 2:
+        API = VERIFY2_API
+        URL = VERIFY2_URL
+    elif int(num) == 3:
+        API = VERIFY3_API
+        URL = VERIFY3_URL
+    else:
+        API = VERIFY4_API
+        URL = VERIFY4_URL
+    
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+
+    if URL == "api.shareus.in":
+        url = f"https://{URL}/shortLink"
+        params = {"token": API,
+                  "format": "json",
+                  "link": link,
+                  }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json(content_type="text/html")
+                    if data["status"] == "success":
+                        return data["shortlink"]
+                    else:
+                        logger.error(f"Error: {data['message']}")
+                        return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+
+        except Exception as e:
+            logger.error(e)
+            return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+    else:
+        url = f'https://{URL}/api'
+        params = {'api': API,
+                  'url': link,
+                  }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json()
+                    if data["status"] == "success":
+                        return data["shortenedUrl"]
+                    else:
+                        logger.error(f"Error: {data['message']}")
+                        if URL == 'clicksfly.com':
+                            return f'https://{URL}/api?api={API}&url={link}'
+                        else:
+                            return f'https://{URL}/api?api={API}&link={link}'
+        except Exception as e:
+            logger.error(e)
+            if URL == 'clicksfly.com':
+                return f'https://{URL}/api?api={API}&url={link}'
+            else:
+                return f'https://{URL}/api?api={API}&link={link}'
+
+
+async def get_token(bot, userid, link, fileid):
+    user = await bot.get_users(userid)
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    TOKENS[user.id] = {token: False}
+    url = f"{link}verify-{user.id}-{token}-{fileid}"
+    await bot.send_message(LOG_CHANNEL, url)
+    status = await get_verify_status(user.id)
+    short_var = status["short"]
+    short_num = int(short_var)
+    if short_num == 4:
+        vr_num = 1
+    else:
+        vr_num = short_num + 1
+    short_verify = await get_verify_shorted_link_first(vr_num, url)
+    short_verify_url = await get_verify_shorted_link_second(vr_num, short_verify)
+    URLINK[user.id] = short_verify_url
+    return str(short_verify_url)
+
+
+async def get_verify_status(userid):
+    status = temp.VERIFY.get(userid)
+    if not status:
+        status = await db.get_verified(userid)
+        temp.VERIFY[userid] = status
+    return status
+
+
+async def check_verification(bot, userid):
+    user = await bot.get_users(int(userid))
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    now = datetime.now(tz)
+    curr_time = now.strftime("%H:%M:%S")
+    hour1, minute1, second1 = curr_time.split(":")
+    curr_time = time(int(hour1), int(minute1), int(second1))
+    status = await get_verify_status(user.id)
+    date_var = status["date"]
+    time_var = status["time"]
+    years, month, day = date_var.split('-')
+    comp_date = date(int(years), int(month), int(day))
+    hour, minute, second = time_var.split(":")
+    comp_time = time(int(hour), int(minute), int(second))
+    if comp_date<today:
+        return False
+    else:
+        if comp_date == today:
+            if comp_time<curr_time:
+                return False
+            else:
+                return True
+        else:
+            return True
