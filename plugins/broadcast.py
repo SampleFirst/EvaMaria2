d3379ec4c6a4147b8a839f7683ffd2bfaf52ffaa
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-29 17:31:56 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -17,7 +17,6 @@ async def broadcast(bot, message):
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
-    blocked = 0
     deleted = 0
     failed =0
     success = 0
@@ -28,27 +27,25 @@ async def broadcast(bot, message):
             if pti:
                 success += 1
             elif pti == False:
-                if sh == "Blocked":
-                    blocked+=1
-                elif sh == "Deleted":
+                if sh == "Deleted":
                     deleted += 1
                 elif sh == "Error":
                     failed += 1
             done += 1
             await asyncio.sleep(2)
             if not done % 20:
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nDeleted: {deleted}")    
         except KeyError as e:
             await sts.edit(f"KeyError: {e}. User object: {user}")
             continue  # Skip this user and continue with the next
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.delete()
-    await bot.send_message(message.chat.id, f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await bot.send_message(message.chat.id, f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nDeleted: {deleted}")
 
 
 async def broadcast_messages(user_id, message):
     try:
-        await message.copy(chat_id=user_id, allow_mentions=True)
+        await message.copy(chat_id=user_id)
         return True, "Success"
     except FloodWait as e:
         await asyncio.sleep(e.value)
@@ -56,13 +53,9 @@ async def broadcast_messages(user_id, message):
     except InputUserDeactivated:
         logging.info(f"{user_id}-Removed from Database, since deleted account.")
         return False, "Deleted"
-    except UserIsBlocked:
-        logging.info(f"{user_id} -Blocked the bot.")
-        return True, "Blocked"
     except PeerIdInvalid:
         await db.delete_user(int(user_id))
         logging.info(f"{user_id} - PeerIdInvalid")
         return False, "Error"
     except Exception as e:
         return False, "Error"
-            
